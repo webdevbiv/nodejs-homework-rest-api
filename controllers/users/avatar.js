3cebc14a931cac126029d900c8b4f22ec27dc234
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-06-29 11:49:15 +0300

    fix

--- controllers/users/avatar.js
+++ controllers/users/avatar.js
@@ -1,24 +1,14 @@
 const Jimp = require("jimp");
 
-const fs = require("fs/promises");
+// const fs = require("fs/promises");
 
 const path = require("path");
 
 const User = require("../../models/user");
 
-const { userAvatarSchema } = require("../../schemas");
-const { HttpError } = require("../../helpers");
-
 const avatarsDir = path.resolve("public", "avatars");
 
 const avatar = async (req, res, next) => {
-  console.log(req.file);
-  const { error } = userAvatarSchema.validate(req.file);
-
-  if (error) {
-    throw HttpError(400, error.message);
-  }
-
   const { path: oldPath, filename } = req.file;
   const newPath = path.join(avatarsDir, filename);
 
@@ -27,7 +17,7 @@ const avatar = async (req, res, next) => {
 
   const userAvatar = path.join("avatars", filename);
 
-  await fs.unlink(oldPath);
+  // await fs.unlink(oldPath);
   const { _id } = req.user;
 
   const result = await User.findByIdAndUpdate(_id, { avatarURL: userAvatar }, { new: true });
--- middlewares/fileExists.js
+++ middlewares/fileExists.js
@@ -1,12 +1,10 @@
-const HttpError = require("../helpers/HttpError");
+const HttpError = require("../helpers");
 
-const fileExists = async (req, file, next) => {
-  // console.log("exists", req.body);
-  // if (!file || file === undefined) {
-  //   next(HttpError(400, "<No image file was sent>"));
-  // }
-  if (!req.files || Object.keys(req.files).length === 0) {
-    next(HttpError(400, "No file was sent."));
+const fileExists = async (req, res, next) => {
+  const file = req.files;
+  console.log("fileExists:", file);
+  if (!file) {
+    next(HttpError(400, "No file uploaded"));
   }
   next();
 };
